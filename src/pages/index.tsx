be96f50aba73f1758a1f6e14a3320870df106861
PolarView <backyardview090@gmail.com>
2023-03-11 14:23:54 +0300

    vers8

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -126,7 +126,6 @@ export default function Home() {
     <>
       <PageContentLayout>
         <>
-          <CreatePostLink />
           {homeFeedLoading ? (
             <Spinner />
           ) : (
